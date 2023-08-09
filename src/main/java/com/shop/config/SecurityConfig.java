9de826913dd55da4da3302550facdaa9007cc6c3
jamirodev <jamirodev@gmail.com>
2023-08-09 17:22:51 +0900

    p.172 // SecurityConfig
    WebSecurityConfigurationAdapter deprecated -> @Bean SecurityFilterChain

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -2,27 +2,56 @@ package com.shop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-        .authorizeRequests()					
-        .antMatchers("/**").permitAll()
-        .antMatchers("/signup").permitAll();
-     return http.build();
-	}
+			.authorizeHttpRequests((authorizeHttpRequests) 
+					-> authorizeHttpRequests
+						.requestMatchers(new AntPathRequestMatcher("/**"))
+						.permitAll())
+			.csrf((csrf) 
+					-> csrf
+						.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+			.headers((headers) 
+					-> headers
+	                	.addHeaderWriter(new XFrameOptionsHeaderWriter(
+	                			XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+			.formLogin((formLogin) 
+					-> formLogin
+	                	.loginPage("/members/login")
+	                	.defaultSuccessUrl("/"))
+			.logout((logout) 
+					-> logout
+	                	.logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+	                	.logoutSuccessUrl("/")
+	                	.invalidateHttpSession(true))
+		;
+    	
+        return http.build();
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 }
--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -2,6 +2,10 @@ package com.shop.service;
 
 import javax.transaction.Transactional;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.shop.entity.Member;
@@ -12,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 	
 	private final MemberRepository memberRepository;
 	
@@ -26,5 +30,19 @@ public class MemberService {
 		if (findMember != null) {
 			throw new IllegalStateException("이미 가입된 회원입니다.");
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Member member = memberRepository.findByEmail(email);
+		if (member == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return User.builder()
+				.username(member.getEmail())
+				.password(member.getPassword())
+				.roles(member.getRole().toString())
+				.build();
 	}
 }
